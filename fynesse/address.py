4efa8b2e61a94528410b05e3d4cc2c510e79675a
FedericoStazi <stazi.federico@gmail.com>
2021-12-02 17:45:46 +0000

    Docstring improvements

--- fynesse/address.py
+++ fynesse/address.py
@@ -1,13 +1,12 @@
-# This file contains code for supporting addressing questions in the data
+""" This file contains code for supporting addressing questions in the data """
 
 import pandas as pd
 import numpy as np
 
 
-
 def one_hot_encoding(df, column, *, values=None):
-    """ Creates a DataFrame containing one hot encodings of a column
-    :param df: the original DataFrame
+    """ Creates a dataframe containing one hot encodings of a column
+    :param df: the original dataframe
     :param column: the name of the column
     :param values: the values column can take
     """
@@ -16,8 +15,12 @@ def one_hot_encoding(df, column, *, values=None):
     return pd.DataFrame({f"{column}_is_{val}": (df[column] == val).astype(float) for val in values})
 
 
-def make_design(columns, data):
+def make_design(columns, df):
+    """ Create a design matrix from a dataframe and a list of its columns
+    :param columns: the list of columns in the dataframe used in the design
+    :param df: the dataframe
+    """
     design = []
     for col in columns:
-        design.append(data[col].values.reshape(-1, 1))
+        design.append(df[col].values.reshape(-1, 1))
     return np.concatenate(design, axis=1)
